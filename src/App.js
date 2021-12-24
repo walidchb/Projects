7a7457bca2109cf2e0a370b9bbfb3cbaf9d79ce9
walid chebbab <walidchebbab2001@gmail.com>
2021-12-24 14:28:46 +0100

    foodie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { Input, Button } from "antd";
 
 function App() {
+  const [foodName, setFoodName] = useState("");
+
+  const voteForFood = () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      food_name: foodName,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost/vote_for_food", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input
+        style={{ width: 200 }}
+        placeholder="vote for your fav food"
+        value={foodName}
+        onChange={(e) => setFoodName(e.target.value)}
+      />
+      <Button type="primary" onClick={voteForFood}>
+        {" "}
+        Vote !{" "}
+      </Button>
     </div>
   );
 }
