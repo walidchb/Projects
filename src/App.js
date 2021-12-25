d84a59f72e185a2bb7fbeec11c8f8c1f8ddfa343
walid chebbab <walidchebbab2001@gmail.com>
2021-12-25 14:10:49 +0100

    statistic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Input, Button } from "antd";
+import { render } from "@testing-library/react";
+import axios from "axios";
+import DemoColumn from "../src/Component/DemoColumn";
 
 function App() {
   const [foodName, setFoodName] = useState("");
+  const [foodData, setfoodData] = useState([]);
+
+  const fetchVotes = () => {
+    var data = "";
+
+    var config = {
+      method: "post",
+      url: "http://localhost/get_foods",
+      headers: {},
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        setfoodData(response.data.votes);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    fetchVotes();
+  }, []);
 
   const voteForFood = () => {
     var myHeaders = new Headers();
@@ -23,9 +50,22 @@ function App() {
 
     fetch("http://localhost/vote_for_food", requestOptions)
       .then((response) => response.text())
-      .then((result) => console.log(result))
+      .then((result) => {
+        fetchVotes();
+        console.log(result);
+      })
       .catch((error) => console.log("error", error));
   };
+
+  function displayData(posts) {
+    return posts.map((post, index) => (
+      <div className="displayData">
+        <table></table>
+        <h3>{post.food_name}</h3>
+        <h3>{post.num_of_votes}</h3>
+      </div>
+    ));
+  }
 
   return (
     <div className="App">
@@ -39,6 +79,12 @@ function App() {
         {" "}
         Vote !{" "}
       </Button>
+
+      {displayData(foodData)}
+
+      <div className="statistic">
+        <DemoColumn data={foodData} />
+      </div>
     </div>
   );
 }
